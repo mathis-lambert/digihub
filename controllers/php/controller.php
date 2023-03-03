36e011c9fa51d3799211b37a6a8aadf779cc09f4
Clement FAVAREL <clementfavarel.dev@gmail.com>
2023-03-03 14:39:54 +0100

    created the 'connexion.php' page

--- controllers/php/controller.php
+++ controllers/php/controller.php
@@ -3,49 +3,50 @@ include_once './models/Model.php';
 
 class Controller
 {
-    public $model;
-    public $conn;
+   public $model;
+   public $conn;
 
-    public function __construct($conn)
-    {
-        $this->conn = $conn;
-        $this->model = new Model($conn);
-    }
+   public function __construct($conn)
+   {
+      $this->conn = $conn;
+      $this->model = new Model($conn);
+   }
 
-    public function invoke()
-    {
-        //routes digihub app
-        $page = isset(array_keys($_GET)[0]) ? array_keys($_GET)[0] : 'home';
+   public function invoke()
+   {
+      //routes digihub app
+      $page = isset(array_keys($_GET)[0]) ? array_keys($_GET)[0] : 'home';
 
-        $conn = $this->model->getConn();
+      $conn = $this->model->getConn();
 
-        switch ($page) {
-            case 'home':
-                $currentPage = 'Accueil';
-                include 'views/home.php';
-                break;
-            case 'results':
-                $currentPage = 'Résultats';
-                include 'views/results.php';
-                break;
-            case 'view':
-                $currentPage = 'Vue média';
-                $id = $_GET['id'];
-                $media = $this->model->getMedia($id);
-                include 'views/view.php';
-                break;
-            case 'nouveautes':
-                include 'views/nouveautes.php';
-                break;
-            case 'top':
-                include 'views/top.php';
-                break;
-            case 'connexion':
-                include 'views/connexion.php';
-                break;
-            default:
-                include 'views/home.php';
-                break;
-        }
-    }
+      switch ($page) {
+         case 'home':
+            $currentPage = 'Accueil';
+            include 'views/home.php';
+            break;
+         case 'results':
+            $currentPage = 'Résultats';
+            include 'views/results.php';
+            break;
+         case 'view':
+            $currentPage = 'Vue média';
+            $id = $_GET['id'];
+            $media = $this->model->getMedia($id);
+            include 'views/view.php';
+            break;
+         case 'nouveautes':
+            include 'views/nouveautes.php';
+            break;
+         case 'top':
+            include 'views/top.php';
+            break;
+         case 'connexion':
+            $currentPage = 'Connexion';
+            include 'views/connexion.php';
+            break;
+         default:
+            include 'views/home.php';
+            break;
+      }
+   }
 }
---
+++ views/connexion.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<?php
+require_once './includes/head.php';
+?>
+
+<body>
+
+   <?php
+   require_once './includes/searchbar.php';
+   require_once './includes/header.php';
+   ?>
+
+   <form method="post" id="login_form" class="container">
+      <div class="input-div">
+         <label for="email">Email</label>
+         <input type="email" name="email" id="email" placeholder="mail@exemple.com" required />
+         <p class="error_msg" id="mail_error"></p>
+      </div>
+      <div class="input-div">
+         <label for="password">Mot de passe</label>
+         <input type="password" name="password" id="password" placeholder="************" required />
+         <p class="error_msg" id="password_error"></p>
+      </div>
+      <input type="submit" value="Login" id="submitForm" />
+   </form>
+
+   <?php require_once './includes/footer.php'; ?>
+</body>
+
+</html>
