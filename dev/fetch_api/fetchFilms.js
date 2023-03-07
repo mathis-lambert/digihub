88a03973fc090e7abe8da51c0b62a788de677be7
Mathis Lambert <mathis.lambert27@gmail.com>
2023-03-07 15:42:16 +0100

    new dump

--- dev/fetch_api/fetchFilms.js
+++ dev/fetch_api/fetchFilms.js
@@ -1,6 +1,8 @@
 const API_KEY = "5adcad38cc39757e9c61c0350b37326a";
 const BASE_URL = "https://api.themoviedb.org/3/";
 
+const range = document.querySelector(".range_bar");
+
 fetchFilms = async (page) => {
   const data_array = [];
   for (let i = 1; i <= page; i++) {
@@ -11,6 +13,14 @@ fetchFilms = async (page) => {
     data_array.push(data);
   }
   computeData(data_array);
+};
+
+fetchGenres = async () => {
+  const response = await fetch(
+    `${BASE_URL}genre/movie/list?api_key=${API_KEY}&language=fr-FR`
+  );
+  const data = await response.json();
+  return data;
 };
 
 fetchCredits = async (id) => {
@@ -29,49 +39,85 @@ fetchCreditsDetails = async (id) => {
   return data;
 };
 
-sendAuthors = async (authors) => {
-  const response = await fetch("http://localhost/digihub/send_data.php", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ authors: authors }),
-  });
-  const data = await response.text();
-  console.debug(data);
-};
-
-sendMovies = async (movies) => {
-  const response = await fetch("http://localhost/digihub/send_data.php", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ movies: movies }),
-  });
-  const data = await response.text();
-  console.debug(data);
+sendData = async (name, data) => {
+  const response = await fetch(
+    "http://localhost/digihub/dev/fetch_api/send_data.php",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ [name]: data }),
+    }
+  );
+  const returnedData = await response.text();
+  console.debug(returnedData);
 };
 
 computeData = async (data_array) => {
-  const filmArray = [];
-  const directorArray = [];
-  const actorsArray = [];
+  const genreArray = [];
+
+  const genres = await fetchGenres();
+
+  for (let i = 0; i < genres.genres.length; i++) {
+    const genre = genres.genres[i];
+    genreArray.push({
+      genreId: genre.id,
+      genreName: genre.name,
+    });
+  }
+
+  //sendGenres("genres",genreArray);
 
   for (let i = 0; i < data_array.length; i++) {
-    console.info(`page ${i} of ${data_array.length}`);
-
     const page = data_array[i];
 
+    document.querySelector("#status_page").innerHTML = `page ${i + 1}/${
+      data_array.length
+    }`;
+
     for (let j = 0; j < page.results.length; j++) {
-      console.info(`media ${i} of ${page.results.length}`);
+      document.querySelector("#status_film").innerHTML = `film ${j + 1}/${
+        data_array.length * page.results.length
+      }`;
+
+      range.style.width = `${
+        ((i * page.results.length + j) /
+          (data_array.length * page.results.length)) *
+        100
+      }%`;
+
+      const filmArray = [];
+      const directorArray = [];
+      const actorsArray = [];
 
       const result = page.results[j];
       const credits = await fetchCredits(result.id);
       const directors = credits.crew.filter((crew) => crew.job === "Director");
+      const actors = credits.cast;
+
+      for (let k = 0; k < actors.length; k++) {
+        const actor = actors[k];
+        const actorDetails = await fetchCreditsDetails(actor.id);
+
+        let actorExists = actorsArray.find(
+          (actor) => actor.actorId === actorDetails.id
+        );
+        if (actorExists) continue;
+
+        actorsArray.push({
+          actorId: actorDetails.id,
+          actorFullname: actorDetails.name,
+          actorBiography: actorDetails.biography,
+          actorBirthDate: actorDetails.birthday,
+          actorDeathDate: actorDetails.deathday,
+          actorBirthPlace: actorDetails.place_of_birth,
+          actorProfileImage: actorDetails.profile_path,
+          actorDepartment: actorDetails.known_for_department,
+        });
+      }
 
       for (let k = 0; k < directors.length; k++) {
-        console.info(`director ${k} of ${directors.length}`);
         const director = directors[k];
         const directorDetails = await fetchCreditsDetails(director.id);
 
@@ -112,14 +158,27 @@ computeData = async (data_array) => {
         mediaDescription: result.overview,
         mediaPublishingDate: result.release_date,
         mediaYear: result.release_date.split("-")[0],
-        mediaTypeId: 1,
+        mediaTypeId: 2,
         mediaGenres: [...result.genre_ids],
         mediaDirectors: directors.map((director) => director.id),
+        mediaActors: [
+          actors.map((actor) => actor.id),
+          actors.map((actor) => actor.character),
+        ],
       });
+      await sendData("authors", directorArray);
+      await sendData("actors", actorsArray);
+      await sendData("movies", filmArray);
+      console.log(directorArray, actorsArray, filmArray);
     }
   }
-  //sendAuthors(directorArray);
-  //sendMovies(filmArray);
+  console.info("done");
 };
 
-fetchFilms(10);
+const button = document.getElementById("btn");
+const pageNumber = document.getElementById("page_number");
+button.addEventListener("click", () => {
+  button.disabled = true;
+  button.innerHTML = "Chargement...";
+  fetchFilms(pageNumber.value);
+});
--- dev/fetch_api/send_data.php
+++ dev/fetch_api/send_data.php
@@ -1,9 +1,24 @@
 <?php
-require './config/config.php';
+require '../../models/Db.php';
+$conn = Db::getInstance();
 //get json body
 $json = file_get_contents('php://input');
 //decode json
 $json = json_decode($json, true);
+
+function check_if_exists($table, $column, $id, $conn)
+{
+    $sql = "SELECT * FROM $table WHERE $column = :id";
+    $stmt = $conn->prepare($sql);
+    $stmt->bindParam(':id', $id);
+    $stmt->execute();
+    $result = $stmt->fetch(PDO::FETCH_ASSOC);
+    if ($result) {
+        return true;
+    } else {
+        return false;
+    }
+}
 
 if (isset($json['authors'])) {
     $authors = $json['authors'];
@@ -18,18 +33,47 @@ if (isset($json['authors'])) {
         $department = $author['directorDepartment'];
         $birthplace = $author['directorBirthPlace'];
 
-        $sql = "INSERT INTO authors (authorId, authorFirstname, authorLastname, authorBirthdate, authorBiography, authorPicture, authorDepartment, authorBirthplace) VALUES (:authorId, :authorFirstname, :authorLastname, :authorBirthdate, :authorBiography, :authorPicture, :authorDepartment, :authorBirthplace)";
-        $stmt = $conn->prepare($sql);
-        $stmt->bindParam(':authorId', $id);
-        $stmt->bindParam(':authorFirstname', $firstname);
-        $stmt->bindParam(':authorLastname', $lastname);
-        $stmt->bindParam(':authorBirthdate', $birthdate);
-        $stmt->bindParam(':authorBiography', $biography);
-        $stmt->bindParam(':authorPicture', $picture);
-        $stmt->bindParam(':authorDepartment', $department);
-        $stmt->bindParam(':authorBirthplace', $birthplace);
-        $stmt->execute();
+        if (!check_if_exists('authors', 'authorId', $id, $conn)) {
+            try {
+                $sql = "INSERT INTO authors (authorId, authorFirstname, authorLastname, authorBirthdate, authorBiography, authorPicture, authorDepartment, authorBirthplace) VALUES (:authorId, :authorFirstname, :authorLastname, :authorBirthdate, :authorBiography, :authorPicture, :authorDepartment, :authorBirthplace)";
+                $stmt = $conn->prepare($sql);
+                $stmt->bindParam(':authorId', $id);
+                $stmt->bindParam(':authorFirstname', $firstname);
+                $stmt->bindParam(':authorLastname', $lastname);
+                $stmt->bindParam(':authorBirthdate', $birthdate);
+                $stmt->bindParam(':authorBiography', $biography);
+                $stmt->bindParam(':authorPicture', $picture);
+                $stmt->bindParam(':authorDepartment', $department);
+                $stmt->bindParam(':authorBirthplace', $birthplace);
+                $stmt->execute();
+            } catch (PDOException $e) {
+                echo $e->getMessage();
+            }
+        }
     }
+
+    echo json_encode(array('status' => 'success -> Author'));
+}
+
+if (isset($json['genres'])) {
+    $genres = $json['genres'];
+    for ($i = 0; $i < count($genres); $i++) {
+        $genre = $genres[$i];
+        $id = intval($genre['genreId']);
+        $name = $genre['genreName'];
+
+        try {
+            $sql = "INSERT INTO genres (genreId, genreName) VALUES (:genreId, :genreName)";
+            $stmt = $conn->prepare($sql);
+            $stmt->bindParam(':genreId', $id);
+            $stmt->bindParam(':genreName', $name);
+            $stmt->execute();
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+        }
+    }
+
+    echo json_encode(array('status' => 'success -> Genre'));
 }
 
 if (isset($json['actors'])) {
@@ -38,19 +82,38 @@ if (isset($json['actors'])) {
     for ($i = 0; $i < count($actors); $i++) {
         $actor = $actors[$i];
         $id = intval($actor['actorId']);
-        $firstname = $actor['actorFirstname'];
-        $lastname = $actor['actorLastname'];
+        $fullName = $actor['actorFullname'];
         $birthdate = $actor['actorBirthDate'];
+        $deathdate = $actor['actorDeathDate'];
         $biography = $actor['actorBiography'];
         $picture = $actor['actorProfileImage'];
-        $birthplace = $actor['actorBirthPlace'];
+        $department = $actor['actorDepartment'];
+        if (!check_if_exists('actors', 'actorId', $id, $conn)) {
+            try {
+                $sql = "INSERT INTO actors (actorId, actorFullname, actorBirthday, actorDeathday, actorBiography, actorCoverImage, actorMainDepartment) VALUES (:actorId, :actorFullname, :actorBirthdate, :actorDeathdate, :actorBiography, :actorPicture, :actorDepartment)";
+                $stmt = $conn->prepare($sql);
+                $stmt->bindParam(':actorId', $id);
+                $stmt->bindParam(':actorFullname', $fullName);
+                $stmt->bindParam(':actorBirthdate', $birthdate);
+                $stmt->bindParam(':actorDeathdate', $deathdate);
+                $stmt->bindParam(':actorBiography', $biography);
+                $stmt->bindParam(':actorPicture', $picture);
+                $stmt->bindParam(':actorDepartment', $department);
+                $stmt->execute();
+            } catch (PDOException $e) {
+                echo $e->getMessage();
+            }
+        }
     }
+
+    echo json_encode(array('status' => 'success -> Actor'));
 }
 
 if (isset($json['movies'])) {
     $movies = $json['movies'];
     for ($i = 0; $i < count($movies); $i++) {
         $movie = $movies[$i];
+
         $id = intval($movie['mediaId']);
         $title = $movie['mediaTitle'];
         $picture = $movie['mediaCoverImage'];
@@ -63,35 +126,76 @@ if (isset($json['movies'])) {
 
         $genreId = $movie['mediaGenres'];
         $directorId = $movie['mediaDirectors'];
+        $actorId = $movie['mediaActors'];
 
-        $sql = "INSERT INTO medias (mediaId, mediaTypeId, mediaName, mediaDescription, mediaPublishingDate, mediaYear, mediaCoverImage, mediaBackgroundImage) VALUES (:mediaId, :mediaTypeId, :mediaName, :mediaDescription, :mediaPublishingDate, :mediaYear, :mediaCoverImage, :mediaBackgroundImage)";
-        $stmt = $conn->prepare($sql);
-        $stmt->bindParam(':mediaId', $id);
-        $stmt->bindParam(':mediaTypeId', $type);
-        $stmt->bindParam(':mediaName', $title);
-        $stmt->bindParam(':mediaDescription', $description);
-        $stmt->bindParam(':mediaPublishingDate', $releaseDate);
-        $stmt->bindParam(':mediaYear', $year);
-        $stmt->bindParam(':mediaCoverImage', $picture);
-        $stmt->bindParam(':mediaBackgroundImage', $backdropPicture);
-        $stmt->execute();
+        if (!check_if_exists('medias', 'mediaId', $id, $conn)) {
+            try {
+                $sql = "INSERT INTO medias (mediaId, mediaTypeId, mediaName, mediaDescription, mediaPublishingDate, mediaYear, mediaCoverImage, mediaBackgroundImage) VALUES (:mediaId, :mediaTypeId, :mediaName, :mediaDescription, :mediaPublishingDate, :mediaYear, :mediaCoverImage, :mediaBackgroundImage)";
+                $stmt = $conn->prepare($sql);
+                $stmt->bindParam(':mediaId', $id);
+                $stmt->bindParam(':mediaTypeId', $type);
+                $stmt->bindParam(':mediaName', $title);
+                $stmt->bindParam(':mediaDescription', $description);
+                $stmt->bindParam(':mediaPublishingDate', $releaseDate);
+                $stmt->bindParam(':mediaYear', $year);
+                $stmt->bindParam(':mediaCoverImage', $picture);
+                $stmt->bindParam(':mediaBackgroundImage', $backdropPicture);
+                $stmt->execute();
+            } catch (PDOException $e) {
+                echo $e->getMessage();
+            }
+
+
+            echo json_encode(array('status' => 'success -> Movie'));
+        } else {
+            echo json_encode(array('status' => 'error -> Movie already exists, continue filling tables'));
+        }
 
         for ($j = 0; $j < count($genreId); $j++) {
             $genre = $genreId[$j];
-            $sql = "INSERT INTO appartient_genre (appartientGenreId, appartientMediaId) VALUES (:genreId, :mediaId)";
-            $stmt = $conn->prepare($sql);
-            $stmt->bindParam(':mediaId', $id);
-            $stmt->bindParam(':genreId', $genre);
-            $stmt->execute();
+            try {
+                $sql = "INSERT INTO appartient_genre (appartientGenreId, appartientMediaId) VALUES (:genreId, :mediaId)";
+                $stmt = $conn->prepare($sql);
+                $stmt->bindParam(':mediaId', $id);
+                $stmt->bindParam(':genreId', $genre);
+                $stmt->execute();
+            } catch (PDOException $e) {
+                echo $e->getMessage();
+            }
+
+            echo json_encode(array('status' => 'success -> Genres related to movie'));
         }
 
         for ($j = 0; $j < count($directorId); $j++) {
             $director = $directorId[$j];
-            $sql = "INSERT INTO appartient_author (appartientAuthorId, appartientMediaId) VALUES (:authorId, :mediaId)";
-            $stmt = $conn->prepare($sql);
-            $stmt->bindParam(':mediaId', $id);
-            $stmt->bindParam(':authorId', $director);
-            $stmt->execute();
+            try {
+                $sql = "INSERT INTO appartient_author (appartientAuthorId, appartientMediaId) VALUES (:authorId, :mediaId)";
+                $stmt = $conn->prepare($sql);
+                $stmt->bindParam(':mediaId', $id);
+                $stmt->bindParam(':authorId', $director);
+                $stmt->execute();
+            } catch (PDOException $e) {
+                echo $e->getMessage();
+            }
+
+            echo json_encode(array('status' => 'success -> Directors related to movie'));
+        }
+
+        for ($j = 0; $j < count($actorId[0]); $j++) {
+            $actor = $actorId[0][$j];
+            $character = $actorId[1][$j];
+            try {
+                $sql = "INSERT INTO appartient_actor (appartientActor_ID, appartientMedia_ID, actorCharacterName) VALUES (:actorId, :mediaId, :character)";
+                $stmt = $conn->prepare($sql);
+                $stmt->bindParam(':mediaId', $id);
+                $stmt->bindParam(':actorId', $actor);
+                $stmt->bindParam(':character', $character);
+                $stmt->execute();
+            } catch (PDOException $e) {
+                echo $e->getMessage();
+            }
+
+            echo json_encode(array('status' => 'success -> Actors related to movie'));
         }
     }
 }
