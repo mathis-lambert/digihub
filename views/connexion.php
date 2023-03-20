c8429d53a1b3a67d492cc4920acadf581b55fa3b
Clement FAVAREL <clementfavarel.dev@gmail.com>
2023-03-20 08:04:18 +0100

    updated styles of login & register

--- views/connexion.php
+++ views/connexion.php
@@ -12,18 +12,21 @@ require_once './assets/includes/head.php';
    require_once './assets/includes/header.php';
    ?>
 
-   <form method="post" id="login_form" class="container">
-      <div class="input-div">
-         <label for="email">Email</label>
-         <input type="email" name="email" id="email" placeholder="mail@exemple.com" required />
-      </div>
-      <div class="input-div">
-         <label for="password">Mot de passe</label>
-         <input type="password" name="password" id="password" placeholder="************" required />
-      </div>
-      <input type="submit" value="Se connecter" />
-      <a href="./?inscription" style="color: black;">Pas encore inscrit ? Créez votre compte !</a>
-   </form>
+   <div class="container">
+      <form method="post" id="login_form" class="card">
+         <h1 class="form-title">Connexion</h1>
+         <div class="input-div">
+            <label for="email">Email</label>
+            <input type="email" name="email" id="email" placeholder="mail@exemple.com" required />
+         </div>
+         <div class="input-div">
+            <label for="password">Mot de passe</label>
+            <input type="password" name="password" id="password" placeholder="************" required />
+         </div>
+         <input type="submit" class="form-btn" value="Se connecter" />
+         <a href="./?inscription" class="form-link">Pas encore inscrit ? Créez votre compte !</a>
+      </form>
+   </div>
 
    <?php require_once './assets/includes/footer.php'; ?>
    <script src="./controllers/js/connexion.js"></script>
